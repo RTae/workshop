f7be892951e26373c5fee9139df88721b1e0b2cf
RTae <natthanan.bhu@dia.co.th>
2023-01-21 13:19:24 +0700

    refactor : change scheam fk

--- db/01-init.sql
+++ db/01-init.sql
@@ -10,7 +10,7 @@ CREATE SEQUENCE IF NOT EXISTS pocket_id;
 
 CREATE TABLE IF NOT EXISTS TBL_Pockets (
     "id" int4 NOT NULL DEFAULT nextval('pocket_id'::regclass),
-    "accountId" int4 REFERENCES "accounts" ("id"),
+    "accountId" int4 REFERENCES "tbl_accounts" ("id"),
     "name" varchar(255) NOT NULL,
     "amount" float8 NOT NULL DEFAULT 0,
     PRIMARY KEY ("id")
@@ -20,9 +20,9 @@ CREATE SEQUENCE IF NOT EXISTS transaction_id;
 
 CREATE TABLE IF NOT EXISTS TBL_Transactions (
     "id" int4 NOT NULL DEFAULT nextval('transaction_id'::regclass),
-    "accountId" int4 REFERENCES "accounts" ("id"),
-    "fromPocketId" int4 REFERENCES "pockets" ("id"),
-    "toPocketId" int4 REFERENCES "pockets" ("id"),
+    "accountId" int4 REFERENCES "tbl_accounts" ("id"),
+    "fromPocketId" int4 REFERENCES "tbl_pockets" ("id"),
+    "toPocketId" int4 REFERENCES "tbl_pockets" ("id"),
     "amount" float8 NOT NULL,
     "date" timestamp NOT NULL DEFAULT now(),
     PRIMARY KEY ("id")
